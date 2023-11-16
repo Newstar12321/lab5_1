4480d1a18e37e21ab5d6771544806afc7b40590e
Newstar12321 <tazeev.zhenya@mail.ru>
2023-11-16 16:25:19 +0700

    Добавьте файлы проекта.

---
+++ lab5_1/lab5_1.cpp
@@ -0,0 +1,41 @@
+﻿/*
+1.	Дан массив А размера N.
+Найти минимальный элемент из его элементов
+с четными номерами: A2, A4, A6, …
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	const int MAX = 10;
+	int array[MAX];
+	int n, min;
+	cout << "Введите число элементов массива" << endl << "n = ";
+	cin >> n;
+	if ((n < 1) || (n > MAX))
+	{
+		cerr << "Ошибка: размер массива не может быть меньше 1 и не должен превышать 10";
+		return 1;
+	}
+	cout << "Введите элементы массива:" << endl;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << i << ") ";
+		cin >> array[i];
+	}
+
+	min = array[0];
+	for (size_t i = 2; i < n; i += 2)
+	{
+		int mini = array[i];
+		if (mini < min) {
+			min = mini;
+		}
+	}
+	cout << "Минимальный элемент из элементов с четными индексами: " << min;
+}
